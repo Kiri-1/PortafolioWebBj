975b2a98f5a3c4c7de32d76a6f3c0a4e0d9e6488
kiri-1 <belenjesuslp@gmail.com>
2024-05-01 02:00:06 -0300

    componentes y cuerpo mas navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CuerpoComponent } from './components/cuerpo/cuerpo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CuerpoComponent,
+    FooterComponent,
+    SobreMiComponent,
   ],
   imports: [
     BrowserModule,
